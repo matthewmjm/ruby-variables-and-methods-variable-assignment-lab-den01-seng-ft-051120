e95f1026f8e078d43cfd215acf185a92298eb574
matthewmjm <matthewmjm@yahoo.com>
2020-05-12 15:22:07 -0600

    Done.

--- variable.rb
+++ variable.rb
@@ -1,2 +1,5 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
+
+# assign a local variable "greeting" that is equal to "Hello World"
+greeting = "Hello World"
